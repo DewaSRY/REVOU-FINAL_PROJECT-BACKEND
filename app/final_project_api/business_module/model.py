1986b1a66ae33d625c1f32e17102400055b9603e
DewaSRY <sdewa6645@gmail.com>
2024-05-06 19:24:02 +0800

    refactor product module and business module

---
+++ app/final_project_api/business_module/model.py
@@ -0,0 +1,182 @@
+"""_summary_
+"""
+
+from .data import BusinessDate, BusinessCreateData
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.sql import func
+from sqlalchemy import ForeignKey, String, Boolean, DateTime
+from app.model_base_service import db, ModelBaseService
+from app.message_service import MessageService
+from app.final_project_api.util import QueryData, QuerySchema
+from datetime import datetime
+
+
+class BusinessModel(BusinessDate, ModelBaseService["BusinessModel"], db.Model):
+    __tablename__ = "business"
+    id = mapped_column("business_id", String(36), primary_key=True)
+    user_id = mapped_column("user_id", String(36), ForeignKey("user.user_id"))
+    business_name = mapped_column("name", String(50))
+    description = mapped_column("description", String(50))
+    profile_url = mapped_column("profile_url", String(50))
+    is_delete = mapped_column("is_delete", Boolean, default=False)
+    create_at = mapped_column(
+        "created_at", DateTime(timezone=True), server_default=func.now()
+    )
+    update_at = mapped_column(
+        "updated_at", DateTime(timezone=True), onupdate=datetime.now
+    )
+    business_type_id = mapped_column(
+        "business_type_id", String(36), ForeignKey("business_type.id")
+    )
+
+    @property
+    def business_images(self):
+        from .image_model import BusinessImageModel
+
+        imagesList = BusinessImageModel.get_image_by_business_id(self.id)
+        if len(imagesList) == 0:
+            return []
+        return imagesList
+
+    @property
+    def product(self):
+        from app.final_project_api.model.product import ProductModel as PM
+
+        return PM.get_by_business_id(business_id=self.id)
+
+    @property
+    def business_types(self):
+        from .type_model import BusinessTypeModel as BTM
+
+        model: BTM = BTM.get_model_by_id(model_id=self.business_type_id)
+        return model.name
+
+    @property
+    def user_phone_number(self):
+        from app.final_project_api.model.user import UserModel
+
+        return UserModel.get_model_by_id(model_id=self.user_id).phone_number
+
+    @property
+    def username(self):
+        from app.final_project_api.model.user import UserModel
+
+        return UserModel.get_model_by_id(model_id=self.user_id).username
+
+    @property
+    def user_email(self):
+        from app.final_project_api.model.user import UserModel
+
+        return UserModel.get_model_by_id(model_id=self.user_id).email
+
+    def _update(
+        self,
+        business_name: str = "",
+        business_type_name: str = "",
+        description: str = "",
+    ) -> None:
+        if len(business_name) != 0:
+            self.business_name = business_name
+        if len(business_type_name) != 0:
+            self.business_type_name = business_type_name
+        if len(description) != 0:
+            self.description = description
+
+    def _set_match_business_type(self, business_type_name: str):
+        from .type_model import BusinessTypeModel
+
+        businessType: BusinessTypeModel = BusinessTypeModel.get_match_model_by_name(
+            model_name=business_type_name
+        )
+        if businessType == None:
+            message = MessageService.get_message("business_typ_not_found").format(
+                f"{business_type_name} not in available list:   "
+            )
+            raise Exception(message)
+        self.business_type_id = businessType.id
+
+    def _get_all_model(self):
+        return self.session.query(BusinessModel).all()
+
+    def _get_model_by_id(self, model_id: str) -> "BusinessModel":
+        return (
+            self.session.query(BusinessModel)
+            .filter(BusinessModel.id == model_id)
+            .first()
+        )
+
+    @classmethod
+    def delete_by_id(cls, model_id: str):
+        from app.final_project_api.model.product import ProductModel
+
+        businessModel: BusinessModel = BusinessModel.get_model_by_id(model_id=model_id)
+        businessModel.is_delete = True
+        productList: list[ProductModel] = businessModel.product
+        for product in productList:
+            product.is_delete = True
+
+        cls.session.add(businessModel)
+        cls.session.commit()
+        return businessModel
+
+    @classmethod
+    def update_by_id(cls, business_id: str, update_data: BusinessCreateData):
+        businessModel: BusinessModel = BusinessModel.get_model_by_id(
+            model_id=business_id
+        )
+        businessModel._update(
+            business_name=update_data.business_name,
+            business_type_name=update_data.business_types,
+            description=update_data.description,
+        )
+        cls.session.add(businessModel)
+        cls.session.commit()
+        return businessModel
+
+    @classmethod
+    def get_all_public_model(cls, query_data: QueryData = QueryData()):
+        queryPointer = cls.session.query(BusinessModel)
+        if len(query_data.search) != 0:
+            return (
+                queryPointer.filter(BusinessModel.is_delete == False)
+                .filter(BusinessModel.business_name.like(query_data.search))
+                .all()
+            )
+        offset = (query_data.page - 1) * query_data.limit
+        return (
+            queryPointer.filter(BusinessModel.is_delete == False)
+            .limit(query_data.limit)
+            .offset(offset)
+            .all()
+        )
+
+    @classmethod
+    def get_by_user_id(cls, user_id: str) -> list["BusinessModel"]:
+        return (
+            cls.session.query(BusinessModel)
+            .filter(BusinessModel.user_id == user_id)
+            .all()
+        )
+
+    @classmethod
+    def add_model(
+        cls,
+        user_id: str,
+        business_name: str,
+        business_type_name: str,
+        description: str,
+    ) -> "BusinessModel":
+
+        model: BusinessModel
+        try:
+            model = BusinessModel(
+                user_id=user_id,
+                business_name=business_name,
+                business_type_name=business_type_name,
+                description=description,
+            )
+        except Exception as e:
+            raise e
+        cls.session.add(model)
+        cls.session.commit()
+        return model
---
+++ app/final_project_api/product_module/views.py
@@ -0,0 +1,317 @@
+"""_summary_
+"""
+
+from http import HTTPStatus
+
+from flask_smorest import abort, Blueprint
+from flask.views import MethodView
+from flask_jwt_extended import jwt_required
+from flask_smorest.fields import Upload
+
+from app.image_upload_service import (
+    ImageService,
+    ImageSchema,
+    ImageData,
+    ImagesPayloadData,
+)
+from app.jwt_service import getCurrentAuthId
+from app.message_service import MessageService
+
+from app.final_project_api.model.product import (
+    ProductData,
+    ProductImageData,
+    ProductImageModel,
+    ProductModel,
+    ProductCreateData,
+    ProductUpdateData,
+)
+from app.final_project_api.model.business import BusinessModel
+from app.final_project_api.model.user import UserModel
+from app.final_project_api.util import QuerySchema, QueryData
+
+from .schema import (
+    ProductSchema,
+    ProductModelSchema,
+    ProductPublicSchemas,
+    ProductCreateSchema,
+    ProductImageModelSchema,
+    ProductWithImageModel,
+    ProductUpdateSchema,
+)
+
+
+blp = Blueprint(
+    "product",
+    __name__,
+    url_prefix="/api/product",
+    description="manage product user product ",
+)
+
+
+@blp.route("/")
+class ProductViews(MethodView):
+
+    @blp.arguments(schema=QuerySchema, location="query")
+    @blp.response(schema=ProductPublicSchemas(many=True), status_code=HTTPStatus.OK)
+    def get(self, query_data: QueryData):
+        """As a user, i can get all product event without access token"""
+        return ProductModel.get_all_public_model(query_data=query_data)
+
+    @jwt_required()
+    @blp.arguments(schema=ProductCreateSchema)
+    @blp.response(schema=ProductModelSchema, status_code=HTTPStatus.CREATED)
+    @blp.alt_response(
+        status_code=HTTPStatus.NOT_FOUND,
+        description=MessageService.get_message("business-not-found").format(
+            "on data payload"
+        ),
+    )
+    @blp.alt_response(
+        status_code=HTTPStatus.NOT_ACCEPTABLE,
+        description=MessageService.get_message("user_id_on_business_not_match").format(
+            "on data payload"
+        ),
+    )
+    def post(self, product_data: ProductCreateData):
+        """As a user, i can create product"""
+        businessModel: BusinessModel = BusinessModel.get_model_by_id(
+            model_id=product_data.business_id
+        )
+        if bool(businessModel) == False or businessModel.user_id != getCurrentAuthId():
+            abort(
+                http_status_code=HTTPStatus.NOT_FOUND,
+                message=MessageService.get_message("business-not-found").format(
+                    product_data.business_id
+                ),
+            )
+        if businessModel.user_id != getCurrentAuthId():
+            abort(
+                http_status_code=HTTPStatus.NOT_ACCEPTABLE,
+                message=MessageService.get_message(
+                    "user_id_on_business_not_match"
+                ).format(businessModel.id),
+            )
+        try:
+            return ProductModel.add_model(
+                business_id=product_data.business_id,
+                product_name=product_data.product_name,
+                product_price=product_data.product_price,
+                description=product_data.description,
+            )
+        except Exception as e:
+            abort(http_status_code=HTTPStatus.CONFLICT, message=str(e))
+
+
+@blp.route("/<string:product_id>")
+class ProductByIdViews(MethodView):
+
+    @blp.response(schema=ProductModelSchema, status_code=HTTPStatus.OK)
+    @blp.alt_response(
+        status_code=HTTPStatus.NOT_FOUND,
+        description=MessageService.get_message("product_not_found").format(
+            "pass ont url"
+        ),
+    )
+    @jwt_required()
+    def get(self, product_id: str):
+        """as a user i can get my product by its id"""
+        productModel: ProductModel = ProductModel.get_model_by_id(model_id=product_id)
+        if bool(productModel) == False or (
+            productModel.delete_model == True
+            or productModel.user_id != getCurrentAuthId()
+        ):
+            abort(
+                http_status_code=HTTPStatus.NOT_FOUND,
+                message=MessageService.get_message("product_not_found").format(
+                    product_id
+                ),
+            )
+
+        return productModel
+
+    @jwt_required()
+    @blp.arguments(schema=ProductUpdateSchema)
+    @blp.response(schema=ProductModelSchema, status_code=HTTPStatus.CREATED)
+    @blp.alt_response(
+        status_code=HTTPStatus.NOT_ACCEPTABLE,
+        description=MessageService.get_message("user_id_on_product_not_match").format(
+            "on url string"
+        ),
+    )
+    def put(self, product_data: ProductUpdateData, product_id: str):
+        """As a user,i can update my product by its id"""
+        productModel: ProductModel = ProductModel.get_model_by_id(model_id=product_id)
+        if productModel.user_id != getCurrentAuthId():
+            abort(
+                http_status_code=HTTPStatus.NOT_ACCEPTABLE,
+                message=MessageService.get_message(
+                    "user_id_on_product_not_match"
+                ).format(product_id),
+            )
+        try:
+            return ProductModel.update_with_update_data(
+                product_id=product_id, product_data=product_data
+            )
+        except Exception as e:
+            abort(http_status_code=HTTPStatus.INTERNAL_SERVER_ERROR, message=str(e))
+
+    @jwt_required()
+    @blp.alt_response(
+        status_code=HTTPStatus.NOT_FOUND,
+        description=MessageService.get_message("product_not_found").format(
+            "pass ont url"
+        ),
+    )
+    def delete(self, product_id: str):
+        """As a user, i can delete my product by its id"""
+        try:
+            deleteModel: ProductModel = ProductModel.delete_model_by_id(
+                model_id=product_id
+            )
+            if bool(deleteModel) == False:
+                abort(
+                    http_status_code=HTTPStatus.NOT_FOUND,
+                    message=MessageService.get_message("product_not_found").format(
+                        product_id
+                    ),
+                )
+            return {"message": f"product with id '{product_id}' success full delete"}
+        except Exception as e:
+            print(str(e))
+            abort(
+                http_status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
+                message=f"failed to delete product with id '{product_id}' ",
+            )
+
+
+# TODO: handle product  image
+@blp.route("/image/<string:product_id>")
+class ImageUserViews(MethodView):
+
+    @jwt_required()
+    @blp.arguments(schema=ImageSchema, location="files")
+    @blp.response(status_code=HTTPStatus.CREATED, schema=ProductImageModelSchema)
+    @blp.alt_response(
+        status_code=HTTPStatus.UNPROCESSABLE_ENTITY,
+        description=MessageService.get_message("file_not_image_type"),
+    )
+    def post(self, data: ImagesPayloadData, product_id: str):
+        """As a user, i can post image to my product"""
+        image_data: Upload = data.image
+        if ImageService.check_extension(image_data=image_data) != True:
+            abort(
+                http_status_code=HTTPStatus.UNPROCESSABLE_ENTITY,
+                message=MessageService.get_message("file_not_image_type"),
+            )
+        response_data: ImageData = ImageService.image_save(image_data=image_data)
+        try:
+            userImageModel = ProductImageModel.add_model(
+                public_id=response_data.public_id,
+                secure_url=response_data.secure_url,
+                product_id=product_id,
+            )
+            return userImageModel
+        except Exception as e:
+            ImageService.image_delete(public_id=response_data.public_id)
+            abort(http_status_code=HTTPStatus.NOT_FOUND, message=str(e))
+
+    @jwt_required()
+    @blp.response(status_code=HTTPStatus.CREATED, schema=ProductWithImageModel)
+    @blp.alt_response(
+        status_code=HTTPStatus.CONFLICT,
+        description=MessageService.get_message("account-not-found").format(
+            "from jwt token"
+        ),
+    )
+    def get(self, product_id: str):
+        """As a user, i can get image by its id"""
+        userModel: ProductModel = ProductModel.get_model_by_id(model_id=product_id)
+        if bool(userModel) == False:
+            abort(
+                http_status_code=HTTPStatus.CONFLICT,
+                message=MessageService.get_message("product_not_found").format(
+                    product_id
+                ),
+            )
+        return userModel
+
+
+@blp.route("/product-image/<string:image_id>")
+class ImageUserViews(MethodView):
+
+    @jwt_required()
+    @blp.response(status_code=HTTPStatus.OK, schema=ProductImageModelSchema)
+    @blp.alt_response(
+        status_code=HTTPStatus.NOT_FOUND,
+        description=MessageService.get_message("image_not_found").format(
+            "pass on url string"
+        ),
+    )
+    def get(self, image_id: str):
+        """As a user, i can get image by its id"""
+        imageMode: ProductImageModel = ProductImageModel.get_model_by_id(
+            model_id=image_id
+        )
+        if bool(imageMode) == False:
+            abort(
+                http_status_code=HTTPStatus.NOT_FOUND,
+                message=MessageService.get_message("image_not_found").format(image_id),
+            )
+        return ProductImageModel.get_model_by_id(model_id=image_id)
+
+    @jwt_required()
+    @blp.response(status_code=HTTPStatus.ACCEPTED)
+    @blp.alt_response(
+        status_code=HTTPStatus.NOT_FOUND,
+        description=MessageService.get_message("image_not_found").format(
+            "pass on url string"
+        ),
+    )
+    def delete(self, image_id: str):
+        """As a user, i can delete image by its id"""
+        imageMode: ProductImageModel = ProductImageModel.get_model_by_id(
+            model_id=image_id
+        )
+        if bool(imageMode) == False:
+            abort(
+                http_status_code=HTTPStatus.NOT_FOUND,
+                message=MessageService.get_message("image_not_found").format(image_id),
+            )
+        userImageModel = ProductImageModel.delete_model_by_id(model_id=image_id)
+        ImageService.image_delete(public_id=userImageModel.public_id)
+        return {"message": f"image with id '{image_id}' delete  success fully"}
+
+    @jwt_required()
+    @blp.response(status_code=HTTPStatus.ACCEPTED, schema=ProductWithImageModel)
+    @blp.alt_response(
+        status_code=HTTPStatus.NOT_FOUND,
+        description=MessageService.get_message("image_not_found").format(
+            "pass on url string"
+        ),
+    )
+    @blp.alt_response(
+        status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
+        description=MessageService.get_message("failed_update_profile").format(
+            "pass on the url string"
+        ),
+    )
+    def put(self, image_id: str):
+        """As a user i can put image by its id to the product profile"""
+        imageMode: ProductImageModel = ProductImageModel.get_model_by_id(
+            model_id=image_id
+        )
+        if bool(imageMode) == False:
+            abort(
+                http_status_code=HTTPStatus.NOT_FOUND,
+                message=MessageService.get_message("image_not_found").format(image_id),
+            )
+        try:
+            return ProductImageModel.put_as_profile(image_id=image_id)
+        except Exception as e:
+            print(e)
+            abort(
+                http_status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
+                message=MessageService.get_message("failed_update_profile").format(
+                    image_id
+                ),
+            )
