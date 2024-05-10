a613853abb470a899277108f7937b8d9fdef788e
DewaSRY <sdewa6645@gmail.com>
2024-05-10 20:27:50 +0800

    remove product image from public product

--- app/final_project_api/product_module/schema.py
+++ app/final_project_api/product_module/schema.py
@@ -53,7 +53,6 @@ class ProductPublicSchemas(Schema):
     update_at = fields.DateTime()
     profile_url = fields.Str()
     business_name = fields.Str(required=True)
-    product_images = fields.List(fields.Nested(ImageModelSchema))
     username = fields.Str(required=True)
     business_id = fields.Str(required=True)
     user_id = fields.Str(required=True)
